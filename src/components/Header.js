06237cbdf738764e1cc9a9bbb64c99159090d490
ayh9601 <ays9499@naver.com>
2021-12-22 10:14:00 +0900

    배포연습

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Logo(){
     return (        
         <h1>
-            <strong>DECODELAB OFFICE</strong><br />
+            <strong>DECODELAB 배포연습</strong><br />
             <span>UI/UX DESIGN&DEVELOPMENT</span>
         </h1> 
     )
